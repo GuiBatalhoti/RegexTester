64b3572b58a14cb90631e2c427406d00baad6b3f
unknown <guibatalhoti@gmail.com>
2022-10-01 12:01:59 -0300

    Parte 01 Finalizada

---
+++ main.js
@@ -0,0 +1,35 @@
+
+
+function regexInput() {
+
+    let inputREGEX = document.getElementById('inputREGEX').value;
+    let inputTest01 = document.getElementById('inputTeste01');
+    let inputTest02 = document.getElementById('inputTeste02');
+  
+    if (inputREGEX[0] === '^' && inputREGEX.at(-1) === "$") {
+        let re = new RegExp(inputREGEX);
+        mudaCorTeste(inputTest01, re.test(inputTest01.value));
+        mudaCorTeste(inputTest02, re.test(inputTest02.value));
+    }
+}
+
+
+function regexTest() {
+    
+    let inputREGEX = document.getElementById('inputREGEX').value;
+    let inputTest01 = document.getElementById('inputTeste01');
+    let inputTest02 = document.getElementById('inputTeste02');
+    let re = new RegExp(inputREGEX);
+    mudaCorTeste(inputTest01, re.test(inputTest01.value));
+    mudaCorTeste(inputTest02, re.test(inputTest02.value));
+}
+
+
+function mudaCorTeste(inputTest, valorRE){
+    if (valorRE == true) {
+        inputTest.style.backgroundColor = 'green';
+    }
+    else {
+        inputTest.style.backgroundColor = 'red';
+    }
+}
